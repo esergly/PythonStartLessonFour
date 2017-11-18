61cb10eaeb4e32d576d76ba8f5bede39d84b7e6a
esergly <29374586+esergly@users.noreply.github.com>
2017-11-18 02:29:26 +0200

    Update LessonThreeTaskTwo.py

--- LessonThreeTaskTwo.py
+++ LessonThreeTaskTwo.py
@@ -1,9 +1,10 @@
-number = int(input('Введите любое целое число больше 4-х, но меньше 16-ти :'))
+number = int(input('Введите любое целое число больше 4-х, но меньше 16-ти: \n'))
 attempt = number
 factorial = 1
 if number < 4 or number > 16:
-    print('Введённое число не соответствует заданному условию.')
-while number >= 1:
-    factorial = factorial * number
-    number = number - 1
+    print('Введённое число не соответствует заданному условию. Попробуйте ещё раз.')
+else:
+    while number >= 1:
+        factorial = factorial * number
+        number = number - 1
 print(str(attempt) + '! = ' + str(factorial))
